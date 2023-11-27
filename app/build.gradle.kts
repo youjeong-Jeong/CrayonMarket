2b878a84186dbe70cb8c8eed10d43050d34b4101
oh in seong <107299318+drimh@users.noreply.github.com>
2023-11-27 14:36:10 +0900

    버전 업데이트 및 navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.crayonmarket"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.crayonmarket"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -42,7 +42,8 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
+    // Base
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -50,6 +51,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
     implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-auth-ktx:22.2.0")
@@ -61,26 +63,31 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-storage:8.0.2")
     implementation("com.google.firebase:firebase-storage-ktx:20.3.0")
 
+    // Coroutine
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 
+    // Gilde
     implementation("com.github.bumptech.glide:glide:4.14.2")
     ksp("com.github.bumptech.glide:ksp:4.14.2")
 
+    // fragment
     debugImplementation("androidx.fragment:fragment-testing:1.6.1")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
 
+    // navigation
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
 
+    // swipe refresh
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    
+
+    // Paging
     implementation("androidx.paging:paging-runtime:3.1.1")
     testImplementation("androidx.paging:paging-common:3.1.1")
     implementation("androidx.paging:paging-rxjava2:3.1.1")
     implementation("androidx.paging:paging-rxjava3:3.1.1")
     implementation("androidx.paging:paging-guava:3.1.1")
-
 }
